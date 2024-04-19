53bca38cf5e60dcdadae8ad165e20dff2ff45543
Juan Cremades <relicary@gmail.com>
2024-04-19 13:45:08 +0200

    Wiring beans using Autowire in the setter
    Refs #2

--- src/main/java/es/relicary/spring_basics/beans/Person.java
+++ src/main/java/es/relicary/spring_basics/beans/Person.java
@@ -8,12 +8,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-@Setter
 @Log4j2
 public class Person {
 
+    @Setter
     private String name;
-    @Autowired
     private Vehicle vehicle;
 
     public Person() {
@@ -25,5 +24,10 @@ public class Person {
         this.name = name;
         this.vehicle = vehicle;
         log.info("Person bean created by Spring: {}", this.name);
+    }
+
+    @Autowired
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
     }
 }
