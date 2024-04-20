843f528a6306c85831fa60e69e21b85c6af4c75f
Juan Cremades <relicary@gmail.com>
2024-04-20 20:12:48 +0200

    Added three methods to the main class
    Refs #6

--- src/main/java/es/relicary/spring_basics/model/Song.java
+++ src/main/java/es/relicary/spring_basics/model/Song.java
@@ -1,10 +1,12 @@
 package es.relicary.spring_basics.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Song {
 
     private String title;
