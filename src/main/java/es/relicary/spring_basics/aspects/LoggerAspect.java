93aae919f8aec38a5695b4065cbd8794109313cf
Juan Cremades <relicary@gmail.com>
2024-04-20 21:09:11 +0200

    AfterReturning and AfterThrowing aspects added
    Refs #6

--- src/main/java/es/relicary/spring_basics/aspects/LoggerAspect.java
+++ src/main/java/es/relicary/spring_basics/aspects/LoggerAspect.java
@@ -1,7 +1,10 @@
 package es.relicary.spring_basics.aspects;
 
 import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
@@ -19,7 +22,7 @@ public class LoggerAspect {
     @Around("execution(* es.relicary.spring_basics.services.*.*(..))")
     public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        log.info("{} method execution start", joinPoint.getSignature().toString());
+        log.info("{} \n\tMethod execution start", joinPoint.getSignature().toString());
 
         Instant start = Instant.now();
         Object result = joinPoint.proceed();
@@ -28,8 +31,18 @@ public class LoggerAspect {
         long timeElapssed = Duration.between(start, finish).toMillis();
         log.info("Time took to execute the method: {}", timeElapssed);
 
-        log.info("{} method execution end", joinPoint.getSignature().toString());
+        log.info("{} \n\tMethod execution end", joinPoint.getSignature().toString());
 
         return result;
+    }
+
+    @AfterThrowing(value = "execution( * es.relicary.spring_basics.services.*.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        log.error("{} \n\tAn exception thrown with the help of @AfterThrowing which happened due to: {}", joinPoint.getSignature().toString(), ex.getMessage());
+    }
+
+    @AfterReturning(value = "execution( * es.relicary.spring_basics.services.*.*(..))", returning = "retVal")
+    public void logStatus(JoinPoint joinPoint, Object retVal) {
+        log.info("{} \n\tMethod successfully processed with the status: {}", joinPoint.getSignature().toString(), retVal.toString());
     }
 }
